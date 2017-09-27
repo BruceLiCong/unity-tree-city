2ffc1e3bae85aa27ff1f2c4684cefc183971f028
Ivan <ia@ivanarellano.com>
2017-09-27 13:30:30 -0400

    Add selector frame to specific canvas

--- MapBoxCity/Assets/Scripts/Modifiers/ObjectInspectorModifier.cs
+++ MapBoxCity/Assets/Scripts/Modifiers/ObjectInspectorModifier.cs
@@ -1,6 +1,7 @@
 namespace TreeCity
 {
     using Mapbox.Unity.MeshGeneration.Components;
+    using Mapbox.Unity.MeshGeneration.Data;
     using Mapbox.Unity.MeshGeneration.Modifiers;
     using UnityEngine;
     using UnityEngine.UI;
@@ -8,21 +9,35 @@ namespace TreeCity
     [CreateAssetMenu(menuName = "TreeCity/Modifiers/Object Inspector Modifier")]
     public class ObjectInspectorModifier : GameObjectModifier
     {
+        private const string CANVAS_NAME = "Canvas";
+        private const string SELECTOR_PATH = "selector";
+
 	    private FeatureUiMarker _marker;
 
-	    public override void Run(FeatureBehaviour fb)
+	    public override void Run(FeatureBehaviour fb, UnityTile tile)
 	    {
 		    if(_marker == null)
 		    {
-			    var canv = FindObjectOfType<Canvas>();
+			    var canvases = FindObjectsOfType<Canvas>();
+                Canvas canv = null;
+
+                foreach (Canvas _canv in canvases)
+                {
+                    if (_canv.name == CANVAS_NAME)
+                    {
+                        canv = _canv;
+                        break;
+                    }
+                }
+
 			    if (canv == null)
 			    {
-				    var go = new GameObject("Canvas", typeof(Canvas), typeof(CanvasScaler), typeof(GraphicRaycaster));
+				    var go = new GameObject(CANVAS_NAME, typeof(Canvas), typeof(CanvasScaler), typeof(GraphicRaycaster));
 				    canv = go.GetComponent<Canvas>();
 				    canv.renderMode = RenderMode.ScreenSpaceOverlay;
 			    }
 
-			    var sel = Instantiate(Resources.Load<GameObject>("selector"));
+			    var sel = Instantiate(Resources.Load<GameObject>(SELECTOR_PATH));
 			    sel.transform.SetParent(canv.transform);
 			    _marker = sel.GetComponent<FeatureUiMarker>();
 		    }
